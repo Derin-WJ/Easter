075441338f9b60af4380a67ca0e6f05ec26f5e3c
Derin-WJ <derinsoysalis2009@gmail.com>
2025-09-03 15:05:18 +0000

    day1

--- Easter.java
+++ Easter.java
@@ -1,5 +1,23 @@
 public class Easter{
-    psvm(String[] args) {
+    public static void main(String[] args) {
         
+
+int y;
+double a == y % 19
+double b == y/100
+double c == y % 100
+double d == b/4
+double e == b%4
+double f == (bt8)/25
+
+
+
+
+
+
+
+
+
+
     }
 }
